9f91081b39f1b482c415280a5b09e131387ef09f
NGUYEN Alexandre <alex.nr@hotmail.fr>
2015-04-10 10:19:00 +0100

    prepare data converter fn

---
+++ dataConverter.js
@@ -0,0 +1,27 @@
+var Checkpoint = require('./models/Checkpoint'),
+    Stop = require('./models/Stop'),
+    Line = require('./models/Line'),
+    TrafficAlert = require('./models/TrafficAlert'),
+    uuid = require('node-uuid'),
+    moment = require('moment');
+
+
+exports.getFormattedDataBus = function(callback) {
+
+
+};
+
+exports.getAllStopsData = function(callback) {
+
+
+};
+
+exports.getCheckpointData = function(titan_code, callback) {
+
+
+};
+
+exports.getTrafficAlertData = function(callback) {
+
+
+};
